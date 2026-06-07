e778c2d02325ad761679784f2219a71dfeca008f
Martin Thompson <martin@dat.ai>
2026-06-07 10:31:22 +0000

    Martin build 2026-06-07

--- platform/src/database.py
+++ platform/src/database.py
@@ -2,9 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from platform.src.models import Base
 
-# Create a database engine and session maker
-engine = create_engine('sqlite:///district_award_travel.db')
+engine = create_engine('sqlite:///award_flights.db')
 Session = sessionmaker(bind=engine)
-
-# Create the database tables
 Base.metadata.create_all(engine)
--- platform/src/models.py
+++ platform/src/models.py
@@ -1,15 +1,12 @@
-class AwardFlightBooking:
-    def __init__(self, id, passenger_name, flight_number, departure_date, return_date, award_points):
-        self.id = id
-        self.passenger_name = passenger_name
-        self.flight_number = flight_number
-        self.departure_date = departure_date
-        self.return_date = return_date
-        self.award_points = award_points
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-class AwardFlightCancellation:
-    def __init__(self, id, award_flight_booking_id, cancellation_date, refund_amount):
-        self.id = id
-        self.award_flight_booking_id = award_flight_booking_id
-        self.cancellation_date = cancellation_date
-        self.refund_amount = refund_amount
+Base = declarative_base()
+
+class AwardFlight(Base):
+    __tablename__ = 'award_flights'
+    id = Column(Integer, primary_key=True)
+    origin = Column(String)
+    destination = Column(String)
+    airline = Column(String)
+    award_availability = Column(Integer)
